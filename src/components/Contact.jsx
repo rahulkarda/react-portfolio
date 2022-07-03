c8f895ade18511085429c784d507a96d49627118
Rahul <rahulkarda2002@gmail.com>
2022-07-03 12:03:40 +0530

    Handling form reset after submit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+//function for form reset
+function handleSubmit(e){
+  e.target.reset();
+}
+
 const Contact = () => {
   return (
     <div
@@ -13,9 +18,10 @@ const Contact = () => {
           </p>
           <p className="py-6">Submit the form below to get in touch with me</p>
         </div>
-
+        
         <div className=" flex justify-center items-center">
           <form
+            onSubmit={handleSubmit}
             name="contact"
             method="POST"
             action="https://getform.io/f/a5bbeab4-3505-457e-bcda-603c8bc15711"
@@ -25,22 +31,25 @@ const Contact = () => {
               type="text"
               name="name"
               placeholder="Enter your name"
+              required
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             />
             <input
-              type="text"
+              type="email"
               name="email"
               placeholder="Enter your email"
+              required
               className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             />
             <textarea
               name="message"
               placeholder="Enter your message"
               rows="10"
+              required
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             ></textarea>
 
-            <button type="submit" className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+            <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
               Send Message
             </button>
           </form>
