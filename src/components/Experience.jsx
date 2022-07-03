9e56f6c48e5e3e023b958a746730572567225a99
Rahul <rahulkarda2002@gmail.com>
2022-07-03 20:37:02 +0530

    Fix contact padding

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -98,7 +98,7 @@ const Experience = () => {
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
             Experience
           </p>
-          <p className="py-6">These are the technologies I've worked with</p>
+          <p className="py-6">These are the technologies I've worked with and used them in my projects.</p>
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
