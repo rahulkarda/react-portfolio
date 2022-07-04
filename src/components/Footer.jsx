03a428bd40c52ebd81c40a35e404a0f9db9052bf
Rahul <rahulkarda2002@gmail.com>
2022-07-04 09:08:31 +0530

    Redesign background

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer(){
     return(
-        <div class="text-center p-6 bg-black mt-10 footer">
+        <div class="text-center p-6 bg-black footer">
         <span class="text-white">&copy; 2022 Copyright - </span>
         <a class="link-underline text-white font-bold" href="https://rahulkarda.netlify.app">Rahul Karda</a>
       </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -39,7 +39,7 @@ const NavBar = () => {
         {links.map(({ id, link }) => (
           <li
             key={id}
-            className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
+            className="nav-links px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 hover:text-white duration-200 link-underline"
           >
             <Link to={link} smooth duration={500}>
               {link}
