273dddd83c9f808cc8d01eacd0347eac23c4e508
Rahul <rahulkarda2002@gmail.com>
2022-07-04 09:14:30 +0530

    Fix form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 //function for form reset
 function handleSubmit(e){
-  e.target.reset();
+  setTimeout(() => {
+    e.target.reset();
+  }, 3000);
 }
 
 const Contact = () => {
@@ -49,7 +51,7 @@ const Contact = () => {
               className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
             ></textarea>
 
-            <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+            <button type="submit" className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
               Send Message
             </button>
           </form>
